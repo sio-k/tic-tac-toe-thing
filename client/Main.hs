f65968e53ea9ad72632cb013e364896749398374
Sio <sioclya@mailbox.tu-berlin.de>
2022-01-30 16:08:55 +0100

    wip, client: well, it's doing something. Also the args parser appears to work right

--- client/Main.hs
+++ client/Main.hs
@@ -8,11 +8,19 @@ import FieldCoords
 import ClientInfo
 import Args
 
-import SDL
-import SDL.Video.Renderer
+import SDL hiding (Vector)
+import SDL.Video.Renderer as Renderer hiding (Vector)
 
 import Data.IORef
-import Data.List
+
+import GHC.Word
+import GHC.Int
+import Data.Vector as Vector hiding (sequence_, copy)
+
+import Foreign.C
+
+import Prelude hiding (null, head, filter, init, tail, head, last, zip, replicate, length)
+import qualified Prelude
 
 main :: IO ()
 main = do
@@ -23,7 +31,7 @@ main = do
                             , windowResizable = True
                             }
   renderer <- createRenderer window
-                             -1
+                             (-1)
                              (RendererConfig AcceleratedVSyncRenderer False)
 
   client <- mkClientInfo renderer args
@@ -33,11 +41,14 @@ main = do
   destroyRenderer renderer
   destroyWindow window
 
+unCInt :: CInt -> Int32
+unCInt (CInt x) = x
+
 mainLoop :: Window -> Renderer -> ClientInfo -> IO ()
 mainLoop window renderer ci = do
-  (V2 w h) <- get $ windowSize window
-  ia <- inputActions window ci w h
-  if not $ null $ filter (== InputQuit) ia
+  (V2 w h) <- fmap unCInt <$> (get $ windowSize window)
+  ia <- fromList <$> inputActions window ci w h
+  if not $ Vector.null $ Vector.filter (== InputQuit) ia
     then do
       error "TODO: notify server that we're quitting and close the connection"
       pure ()
@@ -66,15 +77,15 @@ drawColor = V4 0xBF 0xBF 0xBF 0xFF
 renderState :: Window -> Renderer -> ClientInfo -> Int32 -> Int32 -> IO ()
 renderState window renderer ci w h = do
   let fcs = fieldCoords (boardSize $ config $ sessionGame $ session ci) w h
-  drawLines renderer fcs w h
+  drawFields renderer fcs w h
   drawMarks renderer ci fcs
   drawCurrentTurnIndicator renderer ci
 
-drawLines :: Renderer -> Vector (Vector FieldCoords) -> Int32 -> Int32 -> IO ()
-drawLines renderer fcs w h =
+drawFields :: Renderer -> Vector (Vector FieldCoords) -> Int32 -> Int32 -> IO ()
+drawFields renderer fcs w h =
   let ys = (\ x -> fieldY x + fieldH x) <$> (init $ head <$> fcs)
       xs = (\ x -> fieldX x + fieldW x) <$> (init $ head fcs)
-      mkLineBits x y = Point <$> (uncurry V2) <$> zip (CInt <$> x) (CInt y)
+      mkLineBits x y = P <$> (uncurry V2) <$> zip (CInt <$> x) (CInt <$> y)
       mkNPointsForList l value = replicate (length l) value
       mk0ptsForList l = mkNPointsForList l 0
       mkEndPtsForList l endPoint = mkNPointsForList l endPoint
@@ -88,19 +99,19 @@ drawLines renderer fcs w h =
   in sequence_ $ uncurry (drawLine renderer) <$> lines
 
 drawMarks :: Renderer -> ClientInfo -> Vector (Vector FieldCoords) -> IO ()
-drawMarks renderer ci fields =
+drawMarks renderer ci fields = do
   let allRects = fieldCoordsToSDLRects fields
-      (FrozenBoard bd) = freezeBoard $ board $ sessionGame $ session $ ci
-      bothzipped = uncurry Vector.zip <$> Vector.zip allRects bd -- TODO: this is probably broken
-      tex X = xTex ci
-      tex O = oTex ci
-      draw (_, Nothing) = pure ()
-      draw (rect, (Just x)) = copy renderer (tex x) Nothing $ Just rect
-  in sequence_ $ sequence_ <$> (fmap draw <$> bothzipped)
+  (FrozenBoard bd) <- freezeBoard $ board $ sessionGame $ session $ ci
+  let bothzipped = uncurry Vector.zip <$> Vector.zip allRects bd -- TODO: this is probably broken
+  let tex X = xTex ci
+  let tex O = oTex ci
+  let draw (_, Nothing) = pure ()
+  let draw (rect, (Just x)) = copy renderer (tex x) Nothing $ Just rect
+  sequence_ $ sequence_ <$> (fmap draw <$> bothzipped)
 
 -- top left, 32x32px
 turnIndicatorPos :: Rectangle CInt
-turnIndicatorPos = CInt <$> Rectangle (Point 0 0) 32 32
+turnIndicatorPos = Rectangle (P $ CInt <$> V2 0 0) $ CInt <$> V2 32 32
 
 drawCurrentTurnIndicator :: Renderer -> ClientInfo -> IO ()
 drawCurrentTurnIndicator renderer ci = do
